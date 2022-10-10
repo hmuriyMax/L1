2d39fc4702c42ed5b9bb1f4f50e66a1668b4add6
max <mshchemilkin@gmail.com>
2022-10-10 17:56:01 +0300

    task 11 comments

--- tasks/t11/main.go
+++ tasks/t11/main.go
@@ -2,36 +2,43 @@ package main
 
 import "fmt"
 
+// Set структура множества
 type Set struct {
 	data   *map[interface{}]bool
 	length int
 }
 
+// Init инициализирует множество
 func (s *Set) Init() {
 	newMap := make(map[interface{}]bool)
 	s.data = &newMap
 	s.length = 0
 }
 
+// Add добавлет элемент в множество
 func (s *Set) Add(v interface{}) {
 	(*s.data)[v] = true
 	s.length++
 }
 
+// Remove удаляет элемент из множества
 func (s *Set) Remove(v interface{}) {
 	(*s.data)[v] = false
 	s.length--
 }
 
+// Len возвращает мощность множества
 func (s *Set) Len() int {
 	return s.length
 }
 
+// IsIn проверяет, входит ли элемент во множество
 func (s *Set) IsIn(v interface{}) bool {
 	val, ok := (*s.data)[v]
 	return ok && val
 }
 
+// String возаращает текстовое представление множества
 func (s *Set) String() (out string) {
 	out += "{"
 	for key, val := range *s.data {
@@ -46,6 +53,7 @@ func (s *Set) String() (out string) {
 	return
 }
 
+// Intersection получает множество-пересечение двух
 func Intersection(s1, s2 Set) (res Set) {
 	res.Init()
 	if s2.Len() < s1.Len() {
